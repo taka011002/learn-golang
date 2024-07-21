b553fb04ae2a134f36107dfe05539e99ddb10b55
Takahiro Ishitsuka <taka.rock.i4@gmail.com>
2024-07-22 02:20:59 +0900

    add cloud run

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,11 @@
 FROM golang:1.22.5 as build
 
 WORKDIR /go/src/app
+ARG cmd="cmd/app/main.go"
 COPY . .
 
 RUN go mod download
-RUN CGO_ENABLED=0 go build -o /go/bin/app
+RUN CGO_ENABLED=0 go build -o /go/bin/app $cmd
 
 FROM gcr.io/distroless/static-debian11
 
--- infra/terraform/cloud_run.tf
+++ infra/terraform/cloud_run.tf
@@ -5,27 +5,50 @@ resource "google_cloud_run_service" "api" {
   template {
     spec {
       containers {
-        image = "httpd"
+        image = "asia-northeast1-docker.pkg.dev/playground-430113/learn-golang/api:latest"
+
         ports {
-          container_port = 80
+          container_port = 8080
         }
-        #         env {
-        #           name  = "DB_HOST"
-        #           value = google_sql_database_instance.postgres.connection_name
-        #         }
-        #         env {
-        #           name  = "DB_NAME"
-        #           value = google_sql_database.default.name
-        #         }
-        #         env {
-        #           name  = "DB_USER"
-        #           value = google_sql_user.postgres_user.name
-        #         }
-        #         env {
-        #           name  = "DB_PASSWORD"
-        #           value = google_sql_user.postgres_user.password
-        #         }
+        env {
+          name  = "DB_HOST"
+          value = google_sql_database_instance.postgres.connection_name
+        }
+        env {
+          name  = "DB_NAME"
+          value = google_sql_database.default.name
+        }
+        env {
+          name  = "DB_USER"
+          value = google_sql_user.postgres_user.name
+        }
+        env {
+          name  = "DB_PASSWORD"
+          value = google_sql_user.postgres_user.password
+        }
       }
+
+      // 本当はcloudbuild等でデプロイ時にmigrationを実行したい
+      #       containers {
+      #         image = "asia-northeast1-docker.pkg.dev/playground-430113/learn-golang/migrate:latest"
+      #
+      #         env {
+      #           name  = "DB_HOST"
+      #           value = google_sql_database_instance.postgres.connection_name
+      #         }
+      #         env {
+      #           name  = "DB_NAME"
+      #           value = google_sql_database.default.name
+      #         }
+      #         env {
+      #           name  = "DB_USER"
+      #           value = google_sql_user.postgres_user.name
+      #         }
+      #         env {
+      #           name  = "DB_PASSWORD"
+      #           value = google_sql_user.postgres_user.password
+      #         }
+      #       }
     }
   }
 }
